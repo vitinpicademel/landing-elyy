08af2924df793e064b76c3fedb96e89e38a45cfc
vitinpicademel <novofaceednv@gmail.com>
2025-04-22 09:39:54 -0300

    feat: adiciona arquivo layout-metadata.ts

---
+++ src/app/layout-metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Donna Negócios - Lançamentos Imobiliários',
+  description: 'Conheça os lançamentos imobiliários exclusivos em Franca e Rifaina. Empreendimentos de alto padrão em localizações privilegiadas.',
+} 
